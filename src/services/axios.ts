bf65804bf9a069dbcd25b620394ee525f4abe172
Leonardo Oliveira Balsalobre <oliveirabalsa2@gmail.com>
2025-04-12 10:51:08 -0300

    feat: add axios service configuration

---
+++ src/services/axios.ts
@@ -0,0 +1,51 @@
+import { Task } from "@/components/TaskCard/types";
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://52.204.158.5",
+});
+
+export type AxiosCustomError = {
+  response: {
+    data: {
+      code: string;
+    };
+  };
+};
+
+export const register = async (
+  username: string,
+  email: string,
+  password: string,
+) => api.post("/users/", { username, email, password });
+
+export const login = async (username: string, password: string) =>
+  api.post("/auth/token/", { username, password });
+
+export const createTask = async (task: Task, token: string) =>
+  api.post("/tasks/", task, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const updateTask = async (id: number, task: Task, token: string) =>
+  api.put(`/tasks/${id}/`, task, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const deleteTask = async (id: number, token: string) =>
+  api.delete(`/tasks/${id}/`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const fetchTasks = async (
+  token: string,
+  filters: { [key: string]: string } = {},
+) => {
+  const noEmptyFilters = Object.fromEntries(
+    Object.entries(filters).filter(([, value]) => value),
+  );
+  const queryString = new URLSearchParams(noEmptyFilters).toString();
+  return api.get(`/tasks/?${queryString}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+};
